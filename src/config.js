3e960a37362e12c969ed2c5284ace7d5768dbcc1
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-03-13 16:13:09 -0400

    Setup support for loading environment variables from .env

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 'use strict';
 
-//TODO: read from config
+require('dotenv').config();
+
 module.exports = {
   archerc7: {
     url: process.env.DNS_SCRAPE_HOST,
     username: process.env.DNS_SCRAPE_USERNAME,
-    password: process.env.DNS_SCRAPE_PASSWORD
-  }
+    password: process.env.DNS_SCRAPE_PASSWORD,
+  },
 };
